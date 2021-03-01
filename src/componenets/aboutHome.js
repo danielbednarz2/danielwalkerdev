ed149a4c019d374c90d24bfc6198781f1cd65cae
danielbednarz2 <danielbednarz2@gmail.com>
2021-03-01 06:29:20 -0800

    Updating Technologies

--- src/componenets/aboutHome.js
+++ src/componenets/aboutHome.js
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-import { Html5, Css3, Javascript, NodeDotJs, Gatsby, NextDotJs, Graphql, ReactJs, MaterialUi, Postgresql, Typescript } from "@icons-pack/react-simple-icons";
+import { Html5, Css3, Javascript, NodeDotJs, Gatsby, Graphql, ReactJs, Postgresql } from "@icons-pack/react-simple-icons";
 
 
 const Wrapper = styled.main`
@@ -130,26 +130,12 @@ const AboutHome = () => (
     <Wrapper id="about">
       <About>
         <Info>
-            I am a self-taught developer and have spent the last few years learning the technologies listed here. Feel free to follow the links from the icon to see some projects/articles I've produced in each area of knowledge.
+            As a self-taught developer, I have spent the last few years learning the technologies listed here. Feel free to follow the links from each icon to see projects or articles I've produced in each tenchnogloy.
         </Info>
         <Tech>
             <ul>
                 <a href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5" target="blank" rel="noreferrer">
                     <li><Html5 className="myHtml"/></li>
-                </a>
-                <a href="https://www.javascript.com/" target="blank" rel="noreferrer">
-                    <li><Javascript className="myJS"/></li>
-                </a>
-                <a href="https://nodejs.org/en/" target="blank" rel="noreferrer">
-                    <li><NodeDotJs className="myNode"/></li>
-                </a>
-                <a href="https://www.mongodb.com/" target="blank" rel="noreferrer">
-                    <li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="64" height="64"><path d="M32 24.795c-1.164.296-1.884.013-2.53-.957l-4.594-6.356-.664-.88-5.365 7.257c-.613.873-1.256 1.253-2.4.944l6.87-9.222-6.396-8.33c1.1-.214 1.86-.105 2.535.88l4.765 6.435 4.8-6.4c.615-.873 1.276-1.205 2.38-.883l-2.48 3.288-3.36 4.375c-.4.5-.345.842.023 1.325L32 24.795zM.008 15.427l.562-2.764C2.1 7.193 8.37 4.92 12.694 8.3c2.527 1.988 3.155 4.8 3.03 7.95H1.48c-.214 5.67 3.867 9.092 9.07 7.346 1.825-.613 2.9-2.042 3.438-3.83.273-.896.725-1.036 1.567-.78-.43 2.236-1.4 4.104-3.45 5.273-3.063 1.75-7.435 1.184-9.735-1.248C1 21.6.434 19.812.18 17.9c-.04-.316-.12-.617-.18-.92q.008-.776.008-1.552zm1.498-.38h12.872c-.084-4.1-2.637-7.012-6.126-7.037-3.83-.03-6.58 2.813-6.746 7.037z" className="myExpress"/></svg></li>
-                </a>
-            </ul>
-            <ul>
-                <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="blank" rel="noreferrer">
-                    <li><Css3 className="myCss"/></li>
                 </a>
                 <a href="https://reactjs.org/" target="blank" rel="noreferrer">
                     <li><ReactJs className="myReact" /></li>
@@ -157,22 +143,27 @@ const AboutHome = () => (
                 <a href="https://www.gatsbyjs.com/" target="blank" rel="noreferrer">
                     <li><Gatsby className="myGatsby" /></li>
                 </a>
+            </ul>
+            <ul>
+                <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="blank" rel="noreferrer">
+                    <li><Css3 className="myCss"/></li>
+                </a>
+                <a href="https://nodejs.org/en/" target="blank" rel="noreferrer">
+                    <li><NodeDotJs className="myNode"/></li>
+                </a>
                 <a href="https://www.postgresql.org/" target="blank" rel="noreferrer">
                     <li><Postgresql className="myPostgres"/> </li>
                 </a>
             </ul>
             <ul>
-                <a href="https://material-ui.com/" target="blank" rel="noreferrer">
-                    <li><MaterialUi className="myMaterial" /></li>
+                <a href="https://www.javascript.com/" target="blank" rel="noreferrer">
+                    <li><Javascript className="myJS"/></li>
                 </a>
-                <a href="https://www.typescriptlang.org/" target="blank" rel="noreferrer">
-                    <li><Typescript className="myTypeScript" /></li>
+                <a href="https://www.mongodb.com/" target="blank" rel="noreferrer">
+                    <li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="64" height="64"><path d="M32 24.795c-1.164.296-1.884.013-2.53-.957l-4.594-6.356-.664-.88-5.365 7.257c-.613.873-1.256 1.253-2.4.944l6.87-9.222-6.396-8.33c1.1-.214 1.86-.105 2.535.88l4.765 6.435 4.8-6.4c.615-.873 1.276-1.205 2.38-.883l-2.48 3.288-3.36 4.375c-.4.5-.345.842.023 1.325L32 24.795zM.008 15.427l.562-2.764C2.1 7.193 8.37 4.92 12.694 8.3c2.527 1.988 3.155 4.8 3.03 7.95H1.48c-.214 5.67 3.867 9.092 9.07 7.346 1.825-.613 2.9-2.042 3.438-3.83.273-.896.725-1.036 1.567-.78-.43 2.236-1.4 4.104-3.45 5.273-3.063 1.75-7.435 1.184-9.735-1.248C1 21.6.434 19.812.18 17.9c-.04-.316-.12-.617-.18-.92q.008-.776.008-1.552zm1.498-.38h12.872c-.084-4.1-2.637-7.012-6.126-7.037-3.83-.03-6.58 2.813-6.746 7.037z" className="myExpress"/></svg></li>
                 </a>
-                <a href="https://nextjs.org/" target="blank" rel="noreferrer">
-                    <li><NextDotJs className="myNext" /></li>
-                </a>
-                <a href="https://graphql.org/" target="blank" rel="noreferrer">
-                    <li><Graphql className="myGraph"/> </li>
+                <a href="https://www.https://www.apollographql.com/" target="_blank" rel="noreferrrer">
+                    <li><Graphql className="myGraph" /></li>
                 </a>
             </ul>
         </Tech>
--- src/componenets/projectHome.js
+++ src/componenets/projectHome.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 import netflix from "../images/netflix.jpg"
 import money from "../images/money.jpg"
 import blog from "../images/blog.jpg"
-import { NodeDotJs, Gatsby, NextDotJs, Graphql, ReactJs, MaterialUi, Postgresql} from "@icons-pack/react-simple-icons";
+import { NodeDotJs, Javascript, Graphql, ReactJs, MaterialUi, Postgresql} from "@icons-pack/react-simple-icons";
 
 
 const Wrapper = styled.main`
@@ -189,14 +189,14 @@ const ProjectHome = () => (
             <Description>
                 <h3>Movie App</h3>
                 <Links>
-                <span><a href="www.google.com" target="_blank" rel="noreferrer">Website</a></span>
+                <span><a href="https://www.movieapp.dev" target="_blank" rel="noreferrer">Website</a></span>
                 <span >|</span>
-                <span><a href="www.google.com" target="_blank" rel="noreferrer">Source Code</a></span>
+                <span><a href="https://www.movieapp.dev" target="_blank" rel="noreferrer">Source Code</a></span>
                 </Links>
                 <p>This is my take on a dynamic content platform for watching your favorite shows and movies. I'm writing another sentence because my wife told me to.</p>
                 <TechnologiesIncludes>Technologies included:</TechnologiesIncludes>
                 <ul>
-                    <li><NextDotJs className="myNext" /></li> 
+                <li><ReactJs className="myReact" /></li> 
                     <li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="64" height="64"><path d="M32 24.795c-1.164.296-1.884.013-2.53-.957l-4.594-6.356-.664-.88-5.365 7.257c-.613.873-1.256 1.253-2.4.944l6.87-9.222-6.396-8.33c1.1-.214 1.86-.105 2.535.88l4.765 6.435 4.8-6.4c.615-.873 1.276-1.205 2.38-.883l-2.48 3.288-3.36 4.375c-.4.5-.345.842.023 1.325L32 24.795zM.008 15.427l.562-2.764C2.1 7.193 8.37 4.92 12.694 8.3c2.527 1.988 3.155 4.8 3.03 7.95H1.48c-.214 5.67 3.867 9.092 9.07 7.346 1.825-.613 2.9-2.042 3.438-3.83.273-.896.725-1.036 1.567-.78-.43 2.236-1.4 4.104-3.45 5.273-3.063 1.75-7.435 1.184-9.735-1.248C1 21.6.434 19.812.18 17.9c-.04-.316-.12-.617-.18-.92q.008-.776.008-1.552zm1.498-.38h12.872c-.084-4.1-2.637-7.012-6.126-7.037-3.83-.03-6.58 2.813-6.746 7.037z" className="myExpress"/></svg></li> 
                     <li><Postgresql className="myPostgresql" /></li>
                 </ul>
@@ -206,9 +206,9 @@ const ProjectHome = () => (
             <Description >
             <h3>Financial Planner</h3>
             <Links>
-                <span><a href="www.google.com" target="_blank" rel="noreferrer">Website</a></span>
+                <span><a href="https://www.financialplanner.dev" target="_blank" rel="noreferrer">Website</a></span>
                 <span>|</span>
-                <span><a href="www.google.com" target="_blank" rel="noreferrer">Source Code</a></span>
+                <span><a href="https://www.financialplanner.dev" target="_blank" rel="noreferrer">Source Code</a></span>
             </Links>
             <p>This is my take on a financial planning application.</p>
             <TechnologiesIncludes>Technologies included:</TechnologiesIncludes>
@@ -225,14 +225,14 @@ const ProjectHome = () => (
             <Description >
             <h3>Discover Boldly</h3>
             <Links>
-                <span><a href="www.google.com" target="_blank" rel="noreferrer">Website</a></span>
+                <span><a href="https://www.discoverboldly.com" target="_blank" rel="noreferrer">Website</a></span>
                 <span>|</span>
-                <span><a href="www.google.com" target="_blank" rel="noreferrer">Source Code</a></span>
+                <span><a href="https://www.discoverboldly.com" target="_blank" rel="noreferrer">Source Code</a></span>
             </Links>
             <p>This is my take on a personal blog with a content management system attached.</p>
             <TechnologiesIncludes>Technologies included:</TechnologiesIncludes>
             <ul>
-                <li><Gatsby className="myGatsby" /></li> 
+                <li><Javascript className="myJS" /></li> 
                 <li><ReactJs className="myReact" /></li> 
                 <li><Graphql className="myGraph" /></li>
             </ul>
@@ -242,9 +242,9 @@ const ProjectHome = () => (
             <Description>
             <h3>Givens</h3>
             <Links>
-                <span><a href="www.google.com" target="_blank" rel="noreferrer">Website</a></span>
+                <span><a href="https://www.givens.com" target="_blank" rel="noreferrer">Website</a></span>
                 <span>|</span>
-                <span><a href="www.google.com" target="_blank" rel="noreferrer">Source Code</a></span>
+                <span><a href="https://www.givens.com" target="_blank" rel="noreferrer">Source Code</a></span>
             </Links>
             <p>This is my take on an online shopping application.</p>
             <TechnologiesIncludes>Technologies included:</TechnologiesIncludes>
